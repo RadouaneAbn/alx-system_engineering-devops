b1049e6de966e4b4ab4082f8ffb3e77bf765687f
Radouane Abounouas <redouane1997.ocs@gmail.com>
2024-02-23 20:57:40 +0100

    reorganize the file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-#this script creates a file in /tmp named school
+# this script creates a file in /tmp named school
 
-file {'/tmp/school':
-  ensure  => present,
-  content => 'I love Puppet',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0774'
+file { '/tmp/school':
+ensure  => present,
+mode    => '0774',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
 }
