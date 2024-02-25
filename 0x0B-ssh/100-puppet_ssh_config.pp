47a37946fb16e30fe7f855588179edcdf132926d
Radouane Abounouas <redouane1997.ocs@gmail.com>
2024-02-25 17:34:33 +0100

    Task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,6 @@
 # this script creates sn ssh config file in ~/.ssh/config
 
-file { '/home/red/.ssh/config':
-ensure  => present,
-content => "\
-Host *
-	Hostname 54.236.28.51
-	User ubuntu
-	IdentityFile /home/red/.ssh/school
-",
+exec { 'ssh_config':
+  command => 'echo "Match ubuntu\n  Host *\n    User ubuntu\n    Hostname 54.236.28.51\n    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+  provider => 'shell'
 }
